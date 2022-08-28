5bd610b15baf9673896a8e2e85b529361639daad
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-08-28 23:27:25 +0300

    Time: 4 ms (8.18%), Space: 47.5 MB (23.03%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,15 +1,18 @@
 class Solution {
     public int[] searchRange(int[] nums, int target){
        List<Integer> list=new ArrayList<>();
+        //STORING ELEMENTS INTO ArrayList
        for(int i=0;i<nums.length;i++){
            if(nums[i]==target){
                list.add(i);
            }
        }
+        // STORING ELEMENTS IN ArrayList into Array
         int[] search=new int[list.size()];
         for(int i=0;i<list.size();i++){
             search[i]=list.get(i);
         }
+        // FINDING THE MAX==last-position AND MIN==first-position OF THE OF THE SORTED ARRAY if ArrayList is not empty
         int max=0,min=0;
         if(!list.isEmpty()) {
             max=search[0];
@@ -25,7 +28,9 @@ class Solution {
             }
         }
            }     
+        // assigning min and max into result
         int [] result={min,max};
+        // assigning -1 and -1 into targetNotFoundArray if ArrayList is empty.
         int[] targetNotFoundArray={-1,-1};
         if(!list.isEmpty()){
         return result;
