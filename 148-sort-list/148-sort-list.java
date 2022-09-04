c9306f97d0b2a8ae37c4c4140f0c09b89f59d762
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-09-04 14:31:31 +0300

    Time: 27 ms (19.72%), Space: 74.4 MB (80.69%) - LeetHub

---
+++ 148-sort-list/148-sort-list.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    ListNode anotherBuiltHead;
+    ListNode pointer;
+    private void buildSinglyLinkedList(int data){
+        ListNode node=new ListNode(data);
+        if(anotherBuiltHead==null){
+            anotherBuiltHead=node;
+            pointer=node;
+        }
+        else{
+            pointer.next=node;
+            pointer=node;
+        }
+    }
+    public ListNode sortList(ListNode head) {
+        List<Integer> list=new ArrayList<>();
+        ListNode temp=head;
+        while(temp!=null){
+            list.add(temp.val);
+            temp=temp.next;
+        }
+        Collections.sort(list);
+        for(int i=0;i<list.size();i++){
+            buildSinglyLinkedList(list.get(i));
+        }
+        return anotherBuiltHead;
+    }
+}
