cffdadcc2dab99915f0045e35d2e600a50f0928e
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-09-08 15:48:57 +0300

    Time: 0 ms (100.00%), Space: 43.5 MB (63.51%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.java
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.java
@@ -16,20 +16,18 @@
 class Solution {
    
         public TreeNode sortedArrayToBST(int[] nums) {
-        return CreateBST(nums, 0, nums.length - 1);
+        return CreateBSTFromArray(nums, 0, nums.length - 1);
     }
 
-    private TreeNode CreateBST(int nums[], int l, int r) {
-        if (l > r) { // Base Condition or Recursion Stoping Condition
+    private TreeNode CreateBSTFromArray(int nums[], int left, int right) {
+        if (left > right) { // Base Condition or Recursion Stoping Condition
             return null;
         }
-		// so basically in this question we have to convert sorted array to height balanced tree
-		// so if we directly create tree in given sorted order it will become linked list
-		// so we have to take middle element as head value such it will  become height balanced tree 
-        int mid = (l + r) / 2; 
-        TreeNode root = new TreeNode(nums[mid]); // mid value or median 
-        root.left = CreateBST(nums, l, mid - 1); // assign the value for left of subtree that is l to mid -1 for given array
-        root.right = CreateBST(nums, mid + 1, r); // assign the value for right go subtree that is mid+1 to r for given array
+       // make midpoint a root of TreeNode
+        int midPoint = (left+right) / 2; 
+        TreeNode root = new TreeNode(nums[midPoint]); // mid value or median 
+        root.left = CreateBSTFromArray(nums, left, midPoint - 1); // assign the value for left of subtree that is left to midpoint-1 for given array
+        root.right = CreateBSTFromArray(nums, midPoint + 1, right); // assign the value for right go subtree that is midpoint+1 to right for given array
         return root;
     }
 
