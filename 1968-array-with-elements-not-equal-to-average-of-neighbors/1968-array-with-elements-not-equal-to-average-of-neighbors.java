d8149be525964c2b1db670b46223c02e1b4558b3
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-09-14 14:21:53 +0300

    Time: 51 ms (79.14%), Space: 65.2 MB (87.12%) - LeetHub

--- 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.java
+++ 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
+    //Array With Elements Not Equal to Average of Neighbors
        Arrays.sort(nums);
 	   // sorting in wave format
         for(int i = 0;i<nums.length-1;i+=2){
