2eea3adbc854d9a2da4c74dbaa2d207b2e53b4f3
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-09-14 14:12:35 +0300

    Time: 10 ms (46.93%), Space: 55.6 MB (68.48%) - LeetHub

---
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void wiggleSort(int[] nums) {
+        //creating new array called result and storing all elements of nums into it and sorting it.
+        int[] result = new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+            result[i]=nums[i];
+        Arrays.sort(result);
+        
+        int n = nums.length-1;
+        // updating elements of nums starting from position one and moving by i=i+2     // 0 2 4 6 .....<--index of nums
+        for(int i=1;i<nums.length;i=i+2)
+            nums[i]=result[n--];
+        // updating elements of nums starting from position zero and moving by i=i+2     // 1 3 5 7 ....<--index of nums
+        for(int i=0;i<nums.length;i=i+2)
+            nums[i]=result[n--];
+    }
+}
