b5a3f098e64a4a24a6a170824389dfa1cc0ad756
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-09-18 16:47:00 +0300

    Time: 1 ms (37.03%), Space: 41.7 MB (65.37%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.java
@@ -1,51 +1,40 @@
 class MyStack {
-    //TC of top():O(N)
-    //TC of pop():O(N)
-    //TC of push():O(1)
-    //SC of the two queues:O(2N)-->O(N)
+    //TC of top():O(1)
+    //TC of empty():O(1)
+    //TC of pop():O(1)
+    //TC of push():O(N)
+    //SC of the two queues:O(N)
   private Queue<Integer> q1;
   private Queue<Integer> q2;
     public MyStack() {
         q1=new LinkedList<>();
         q2=new LinkedList<>();
-    }
+    }   
     public void push(int x) {
-        q1.add(x);
+        q2.add(x);
+        //push all the remaining elements in q1 to q2
+        while(!q1.isEmpty()){
+            q2.add(q1.peek());
+            q1.remove();
+        }
+        //swap the names of two queues
+        //avoid copying all elements from q2 to q1.
+        Queue<Integer> q=q1;
+        q1=q2;
+        q2=q;
     }
     
     public int pop() {
-        int n=q1.size();
-        while(n>1){
-            int e=q1.peek();
-            q2.add(e);
-            q1.remove();
-            n--;
+        if(q1.isEmpty()){
+            return 0;
         }
         int x=q1.peek();
         q1.remove();
-        while(!q2.isEmpty()){
-            q1.add(q2.peek());
-            q2.remove();
-        }
         return x;
     }
     
     public int top() {
-        int n=q1.size();
-        while(n>1){
-            int e=q1.peek();
-            q2.add(e);
-            q1.remove();
-            n--;
-        }
-        int x=q1.peek();
-        q1.remove();
-        while(!q2.isEmpty()){
-            q1.add(q2.peek());
-            q2.remove();
-        }
-        q1.add(x);
-        return x;
+        return q1.peek();
     }
     
     public boolean empty() {
