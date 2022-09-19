634f8c88d34dd3e810624c415d25a4d46e072660
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-09-19 22:19:54 +0300

    Time: 10 ms (13.19%), Space: 81.5 MB (23.10%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -9,7 +9,7 @@ class Solution {
         //1.height represent the height of the water
         //2.omega-alfa represent the base of the water
         //3.The product of the height and omega-alfa give us an area
-        //4.To get the maximum area we have to iterate through the vertical lines by using Math.max() method
+        //4.To get the maximum area we have to iterate through the vertical lines which gives us the maximum area(i.e maximum area is found between the vertical lines height[alfa] and height[omega])
         while(alfa<omega){
             if(height[alfa]<height[omega]){
                 maxArea=Math.max(maxArea,height[alfa]*(omega-alfa));
