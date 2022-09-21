1e99968c1d99e2ecc6e7557379b6707265c6e82a
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-09-21 14:32:33 +0300

    Time: 9 ms (22.76%), Space: 44.3 MB (52.45%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,17 +1,36 @@
+// nick white youtube
+// Optimised Solution TC: O(n^2) 
+//                    SC: O(n)
 class Solution {
-    // nick white youtube
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> nextGreater=new HashMap<>();
-        Stack<Integer> stack=new Stack<>();
-        for(Integer num:nums2){
-            while(!stack.isEmpty() && stack.peek()<num){
-                nextGreater.put(stack.pop(),num);
+        
+        // We Store The Values Of Element And Its Next Greater Element In The HashMap
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        // We Use Stack To Use LIFO Property In Filling The HashMap
+        Stack<Integer> stack = new Stack();
+        
+        // Iterating Through nums2
+        // Filling The Map Using Stack
+        for(int num :nums2){
+            //1.At the very beginning before going for while loop the element is pushed to stack since stack is empty
+            
+            // For Every Element That Is Smaller Than Current Element, We Pop Its Value With The Element On Stack As The Key
+            while(!stack.isEmpty() && stack.peek() < num){
+                map.put(stack.pop(), num);
             }
+            
+            // Pushing Current Element In The Stack
             stack.push(num);
         }
-        for(int i=0;i<nums1.length;i++){
-            nums1[i]=nextGreater.getOrDefault(nums1[i],-1);
+        
+        // Iterating Through nums1 And Checking For Element In HashMap As Key And If It Has A Greater Value Or Not
+        for(int i=0; i<nums1.length; i++){
+            nums1[i] = map.getOrDefault(nums1[i], -1);
         }
+        
+        // Returning The Modified Array
         return nums1;
+        
     }
 }
