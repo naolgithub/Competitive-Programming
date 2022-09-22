c1869de1219089bf5352f9e35a373a23829bde93
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-09-22 10:07:12 +0300

    Time: 169 ms (47.28%), Space: 134.7 MB (7.46%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -4,14 +4,14 @@ class Solution {
      int[] map=new int[temperatures.length];
         for(int i=0;i<temperatures.length;i++){
             while(!stack.isEmpty() && temperatures[stack.peek()]<temperatures[i]){
-                Integer topIndex=stack.pop();
+                int topIndex=stack.pop();
                 int diff=i-topIndex;
                 map[topIndex]=diff;
             }
             stack.push(i);
         }
         while(!stack.isEmpty()){
-            Integer topIndex=stack.pop();
+            int topIndex=stack.pop();
             map[topIndex]=0;
         }
         return map;
