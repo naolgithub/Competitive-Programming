d0be4d4adbd77c6e268c6dcb9b249aa79980a8e5
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-09-22 16:26:08 +0300

    Time: 1 ms (85.05%), Space: 43.7 MB (46.19%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.java
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.java
@@ -10,21 +10,24 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode dummy=new ListNode(0,head);
-        ListNode prev=dummy;
+        ListNode naolDummy=new ListNode(0,head);
+        ListNode prev=naolDummy;
         while(head!=null){
-            if(head.next!=null&&head.val==head.next.val){
-                while(head.next!=null&&head.val==head.next.val){
+            
+            if(head.next!=null && head.val==head.next.val){
+                while(head.next!=null && head.val==head.next.val){
                     head=head.next;
                 }
                 prev.next=head.next;
             }
             
-            else {
+            else{
                 prev=prev.next;
             }
+            
             head=head.next;
         }
-        return dummy.next;
+        
+        return naolDummy.next;
     }
 }
