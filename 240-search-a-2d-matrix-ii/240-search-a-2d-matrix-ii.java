232b11694f4ebdc5c2995754220b27455eb24325
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-09-23 11:02:31 +0300

    Time: 5 ms (100.00%), Space: 48 MB (97.82%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -1,14 +1,29 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-         int row=0;
+        /*
+        We need to start with the greatest column and work our way to the left. This means that our first element
+for comparison is matrix[0][c-1], where c is the number of columns.
+       NOTE:Element may not be at the end of a row in the full matrix, but it is at an end of a row of a submatrix. The
+same conditions apply. Element may not be at the start of a column in the full matrix,but it is at an start of a column of a submatrix              //1.WE START AT THE GREATEST COLUMN , IN THE FIST ROW
+          //2.IF NOT WE WORK OUR WAY TO THE LEFT(WE DECREASE OUR VALUE OF COLUMN)
+          //3.IF NOT WE WORK OUR WAY TO THE DOWN(WE INCREASE THE VALUE OF ROW SINCE WE STARTED FROM FIRST ROW)
+        */
+        
+        
+        
+        //1.WE START AT THE GREATEST COLUMN , IN THE FIST ROW
+        int row=0;
         int column=matrix[0].length-1;
+        
         while(row<matrix.length && column>=0){
             if(matrix[row][column]==target){
                 return true;
             }
+            //2.IF NOT WE WORK OUR WAY TO THE LEFT(WE DECREASE OUR VALUE OF COLUMN)
             else if(matrix[row][column]>target){
                 column--;
             }
+            //3.IF NOT WE WORK OUR WAY TO THE DOWN(WE INCREASE THE VALUE OF ROW SINCE WE STARTED FROM FIRST ROW)
             else{
                 row++;
             }
