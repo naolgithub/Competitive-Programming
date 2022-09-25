d8bc2267cf4941520ec56d7429237e12afb2fcdf
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-09-25 22:07:10 +0300

    Time: 0 ms (100.00%), Space: 40.1 MB (96.52%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -10,18 +10,21 @@ class Solution {
             
             for(int i=columnBegin;i<=columnEnd;i++){
                 result.add(matrix[rowBegin][i]);
-            }
+            }         
             rowBegin++;
+            
             for(int i=rowBegin;i<=rowEnd;i++){
                 result.add(matrix[i][columnEnd]);
             }
             columnEnd--;
+            
             if(rowBegin<=rowEnd){
                 for(int i=columnEnd;i>=columnBegin;i--){
                     result.add(matrix[rowEnd][i]);
                 }
             }
             rowEnd--;
+            
             if(columnBegin<=columnEnd){
                 for(int i=rowEnd;i>=rowBegin;i--){
                     result.add(matrix[i][columnBegin]);
