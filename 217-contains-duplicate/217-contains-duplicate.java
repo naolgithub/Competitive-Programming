ed78471984983a362f26c1f50a39eec21769bcd3
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-09-27 14:03:17 +0300

    Time: 26 ms (28.83%), Space: 71.3 MB (22.87%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+        // using HashSet
      HashSet<Integer> set=new HashSet<>();
         for(int num:nums){
             if(set.contains(num)) return true;
