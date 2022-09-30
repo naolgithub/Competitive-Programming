bdec102419cd19c81ae9f561756d484b55a37f65
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-09-30 17:19:43 +0300

    Time: 9 ms (32.51%), Space: 44.6 MB (29.85%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i:nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        for(int key:map.keySet()){
+            if(map.get(key)==1){
+                return key;
+            }
+        }
+        return -1;
+    }
+}
