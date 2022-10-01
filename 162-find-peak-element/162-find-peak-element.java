505fb3fcd1db01f358a8cb11aa89859b923aa0e0
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-01 11:35:12 +0300

    Time: 0 ms (100.00%), Space: 43.1 MB (33.74%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -4,10 +4,10 @@ class Solution {
       int right = nums.length - 1;
         while (left < right) {
             int mid = left+(right-left) / 2;// This protects overflow
-            if (nums[mid] > nums[mid + 1])
-                right = mid;
+            if (nums[mid] < nums[mid + 1])
+                left = mid+1;
             else
-                left = mid + 1;
+                right=mid;
         }
         return left;  
     }
