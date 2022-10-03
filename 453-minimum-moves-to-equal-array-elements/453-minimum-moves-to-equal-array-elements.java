de47e02c4eee7c4b23cb5fecb1877990aff894f4
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-03 12:38:18 +0300

    Time: 11 ms (31.31%), Space: 54 MB (58.55%) - LeetHub

--- 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.java
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.java
@@ -1,4 +1,7 @@
 class Solution {
+    /*
+    https://leetcode.com/problems/minimum-moves-to-equal-array-elements/discuss/2217319/Java-easy-solution-O(n)-Similar-Question%2B
+    */
     // Here, we can increment only n-1 elements so, just take the minimum and find out absolute sum difference of each from minimum.
     // tc:O(n)
     //sc: O(1)
