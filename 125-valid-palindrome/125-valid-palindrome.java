f63710c4014332c6097bef19be3d877eea7e3f32
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-04 22:53:25 +0300

    Time: 12 ms (48.92%), Space: 44.4 MB (43.90%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -0,0 +1,27 @@
+class Solution {
+    /*
+    https://www.techiedelight.com/remove-non-alphanumeric-characters-from-string-java/
+    */
+    public boolean isPalindrome(String s) {
+        //METHOD ONE
+        s = s.toLowerCase();
+        StringBuilder stringBuilder = new StringBuilder();
+        // adding only numbers and letters to stringBuilder
+        for(int i = 0; i<s.length(); i++){
+            if(Character.isDigit(s.charAt(i)) || Character.isLetter(s.charAt(i))){
+                stringBuilder.append(s.charAt(i));
+            }
+        }
+        int n = stringBuilder.length()-1;
+        for(int i = 0; i<stringBuilder.length()/2; i++){
+            if(stringBuilder.charAt(i)!= stringBuilder.charAt(n-i))return false;
+        }
+         return true;   
+        //METHOD TWO
+        /*
+        String actual = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        String reversed = new StringBuffer(actual).reverse().toString();
+        return actual.equals(reversed);
+        */
+    }
+}
