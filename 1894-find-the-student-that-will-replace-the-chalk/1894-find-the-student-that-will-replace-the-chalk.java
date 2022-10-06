54768c6502e9a2cc15188419bd7d86e961e4d8a0
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-06 12:39:40 +0300

    Time: 1 ms (100.00%), Space: 55.3 MB (91.95%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
@@ -1,35 +1,26 @@
-/*Since we know every chalk is consumed fully we can cancel the iterations that the teacher makes from begining to end by assiging them to students. Lets see how.
-
-So we know when the teacher starts distributing chalks and does that till the end of array she has distributed
-
-= chalk[0] + chalk[1] + chalk[2] + .... + chalk[n-2] + chalk[n-1]
-
-and she again starts from index 0.
-
-Now lets say , chalk[0] + chalk[1] + chalk[2] + .... + chalk[n-2] + chalk[n-1] = SUM
-
-So, if teacher peforms the cycle of distribution of chalks from begining to end Y times ,
-the total chalks distributed = SUM * Y
-
-after all cycles she is left with how many chalks ?
-
-remaining = ( k - (SUM * Y) ) = k % SUM
-
-thus we can simply compute this remainder
-and then iterate over the chalks array and check which student cannot get the chalk now.
+/*
+1.Let's take sumas thesum of chalk[i] from i in range[0, n - 1]. This will store the total chalk used in a single process.
+2.We have to only look when the chalk will be replaced where k = k % sum. Because after each k / sum times, the process will restart again from 0th student.
+3.We travserse the chalk array and add the current sum till ith index, we will return the index when current sum becomes greater than k % sum for the first time.
 */
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
-		long sum = 0;
-		for (int eachChalk : chalk) sum+=eachChalk;
-		long remainder = k%sum;
-
-		for (int i= 0 ; i<chalk.length ; i++) {
-			if (remainder>=chalk[i] )
-				remainder-=chalk[i];
-			else
-				return i;
-		}
-		return -1;
-	}
+       
+        long sum = 0;
+        long currentSum = 0;
+        
+        for(int eachChalk : chalk)
+            sum += eachChalk;
+        
+        sum = k % sum;
+        
+        for(int i = 0; i < chalk.length; ++i) {
+            currentSum += chalk[i];
+            
+            if(currentSum > sum)
+                return i;
+        }
+        
+        return - 1;
+    }
 }
