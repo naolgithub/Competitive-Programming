5850c920cf9afc21d64e24eb576cb72e75a0357a
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-09 11:47:06 +0300

    Time: 12 ms (5.09%), Space: 45.1 MB (5.39%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,19 +1,16 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
-        // using array : tc:O(N^2)
-        //               sc:O(1)
-        for(int i=0;i<prices.length;i++){
-           //when we reach the last ith item, we break             //out of the loop
-            if(i+1==prices.length)break;
-            
-            for(int j=i+1;j<prices.length;j++)
-            {
-                if(prices[j]<=prices[i])
-                {
-                    prices[i]=prices[i]-prices[j];
-                    break;
-                }
+     Stack<Integer> stack = new Stack<>();
+        for (int i=prices.length-1; i>=0; i--) {
+            //if peek of the stack is greater than price which is going to be added, we remove peek of stack
+    while(!stack.isEmpty() && stack.peek()>prices[i]){
+                stack.pop();
             }
+            int price = prices[i];
+            if (!stack.isEmpty()) {
+                prices[i] = prices[i] - stack.peek();  
+            }
+            stack.push(price);
         }
         return prices;
     }
