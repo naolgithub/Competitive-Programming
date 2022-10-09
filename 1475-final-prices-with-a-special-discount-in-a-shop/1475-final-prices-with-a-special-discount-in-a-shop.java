7355b6b91ad55fc0841798b43f696e254cf4e1de
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-09 11:00:24 +0300

    Time: 6 ms (41.52%), Space: 44.8 MB (13.54%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -3,9 +3,6 @@ class Solution {
         // using array : tc:O(N^2)
         //               sc:O(1)
         for(int i=0;i<prices.length;i++){
-           //when we reach the last ith item, we break             //out of the loop
-            if(i+1==prices.length)break;
-            
             for(int j=i+1;j<prices.length;j++)
             {
                 if(prices[j]<=prices[i])
