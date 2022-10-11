6600e3afac99e2c8cc9e21426bc2ec5ee775f144
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-11 18:08:05 +0300

    Time: 5 ms (72.39%), Space: 44.8 MB (66.42%) - LeetHub

--- 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.java
+++ 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.java
@@ -1,9 +1,11 @@
 class Solution {
+   // Time Complexity:O(N^2)
+   // Space Complexity:O(1)
     public int[] sortEvenOdd(int[] nums) {
     //after sorting of evenodd, the answer contains:
     //even indices are sorted in increasing order
     // odd indices are sorted in decreasing order
-            for(int i=1;i<nums.length;i+=2){
+        for(int i=1;i<nums.length;i+=2){
         for(int j=i+2;j<nums.length;j+=2){
             if(nums[i]<=nums[j]){
                 int temp=nums[i];
