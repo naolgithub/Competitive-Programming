6896ed2907f788c20515f61ee82ddc7443a0cb24
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-11 15:34:17 +0300

    Time: 13 ms (75.21%), Space: 54.4 MB (66.67%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -1,5 +1,6 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
+        //HashMap and sorting technique is used
         Map<Integer, String> map = new HashMap();
         for(int i=0; i<heights.length; i++){
             map.put(heights[i], names[i]);
@@ -7,6 +8,9 @@ class Solution {
         Arrays.sort(heights);
         String[] result = new String[heights.length];
         int counter=0;
+        /*
+        Names are sorted in descending order by the people's heights.
+        */
         while(counter<heights.length){
             result[counter] = map.get(heights[heights.length-counter-1]);
             counter++;
