5431dd65e6653630c4f71c8f1a4181cb4b313951
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-11 13:22:22 +0300

    Time: 37 ms (13.40%), Space: 53.8 MB (10.59%) - LeetHub

---
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -0,0 +1,27 @@
+class KthLargest {
+    // link :solution
+    private static int k;
+    private PriorityQueue<Integer> heap;
+    
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        heap = new PriorityQueue<>();
+        
+        for (int num: nums) {
+            heap.offer(num);
+        }
+        
+        while (heap.size() > k) {
+            heap.poll();
+        }
+    }
+    
+    public int add(int val) {
+        heap.offer(val);
+        if (heap.size() > k) {
+            heap.poll();
+        }
+
+        return heap.peek();
+    }
+}
