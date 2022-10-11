af126996c982909a616a158f65721a5eb190ce2c
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-11 17:27:44 +0300

    Time: 2 ms (57.89%), Space: 47.8 MB (79.28%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -3,6 +3,7 @@ class Solution {
         int lastEvenIndex = 0;
         for (int i=0; i< nums.length; i++) {
             if (nums[i] % 2 == 0) {
+                // taking the even numbers to the beginning of nums and the rest will be to the last of nums.........i.e swaping them
                 int temp = nums[lastEvenIndex];
                 nums[lastEvenIndex] = nums[i];
                 nums[i] = temp;
