83cd550443e2507037f7e5bb8d458ad66647e95e
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-11 12:56:25 +0300

    Time: 317 ms (14.36%), Space: 121.5 MB (68.40%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -0,0 +1,37 @@
+/**
+ * Using two heaps
+ * 
+ * Time Complexity:
+ * 1) addNum -> O(5 * log (N/2)) = O(log N)
+ * 2) findMedian -> O(1)
+ *
+ * Space Complexity: O(N)
+ *
+ * N = Count of numbers in the data stream.
+ */
+class MedianFinder {
+    PriorityQueue<Integer> smallNums; // Max Heap
+    PriorityQueue<Integer> largeNums; // Min Heap
+
+    public MedianFinder() {
+        smallNums = new PriorityQueue<>(Collections.reverseOrder());
+        largeNums = new PriorityQueue<>();
+    }
+
+    public void addNum(int num) {
+        // Add to maxHeap first (Group of smaller numbers)
+        smallNums.offer(num);
+        // Balance the heaps
+        largeNums.offer(smallNums.poll());
+        if (largeNums.size() > smallNums.size()) {
+            smallNums.offer(largeNums.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (smallNums.size() != largeNums.size()) {
+            return smallNums.peek();
+        }
+        return (smallNums.peek() + largeNums.peek()) / 2.0;
+    }
+}
