c900b036b8793c23922d42e306ffe1f82939ce61
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-12 16:41:12 +0300

    Time: 1 ms (50.97%), Space: 42.8 MB (7.37%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -1,7 +1,8 @@
 class Solution {
+    public int largestAltitude(int[] gain) {
+        /*
     // METHOD ONE : TC:O(N^2) and SC:O(1)
     // similar to (1480.Running sum of 1d array)
-    public int largestAltitude(int[] gain) {
         int max=0;//Taking the initial max as 0 since biker starts his trip on point 0 with altitute equal to 0
         //then we compare all available net gain in altitude between points i and i+1 for all(0<=i<n)
         for(int i=0;i<gain.length;i++){
@@ -10,6 +11,16 @@ class Solution {
                 sum+=gain[j];
                 max=Math.max(max,sum);
             }
+        }
+        return max;
+        */
+        
+        //METHOD TWO :TC:O(N) and SC:O(1)
+        int max=0;
+        int sum=0;
+        for(int i=0;i<gain.length;i++){
+            sum+=gain[i];
+            max=Math.max(max,sum);
         }
         return max;
     }
