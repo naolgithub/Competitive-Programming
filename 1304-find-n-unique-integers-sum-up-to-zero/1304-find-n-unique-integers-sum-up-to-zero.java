ac02162347a63a25eb8dbb9b47e49ac166380126
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-12 17:01:10 +0300

    Time: 0 ms (100.00%), Space: 40.2 MB (97.00%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.java
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] sumZero(int n) {
-        // using two pointers 
+    // using two pointers 
+    //Return an array where the values are symmetric.(+x , -x).
        int[] result=new int[n];
         int start = 0;
         int end = n - 1;
