08523d72e722b9c4357514820722ae34cf31d6e8
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-13 15:54:31 +0300

    Time: 2 ms (77.84%), Space: 73.7 MB (50.11%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,11 +1,16 @@
 class Solution {
+    // using Kadane's Algorithm solution
+    // TC:O(N)
+    // SC:O(1)
     public int maxSubArray(int[] nums) {
+        
         int maxSum = nums[0];
         int currentSum=maxSum;
+        // we have to start from index 1 since we have already assigned index 0
         for(int i=1;i<nums.length;i++){
             currentSum=Math.max(currentSum+nums[i],nums[i]);
-            maxSum=Math.max(maxSum,currentSum);
+            maxSum=Math.max(currentSum,maxSum); 
         }
-        return maxSum;  
+     return maxSum;   
     }
 }
