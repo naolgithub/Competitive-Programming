11fa7999a520edbf491d35f2ba2e0ad719517acf
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-14 13:05:36 +0300

    Time: 74 ms (22.76%), Space: 67.5 MB (51.44%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,6 +1,22 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        // Time Limit Exceeded Solution
+      // nick white
+      // https://youtu.be/AmlVSNBHzJg
+        
+     Map<Integer,Integer> arraySums=new HashMap<>();
+        int sum=0;
+        int result=0;
+        arraySums.put(0,1);// assuming key 0 has 1 value
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(arraySums.containsKey(sum-k)){
+                result+=arraySums.get(sum-k);
+            }
+            arraySums.put(sum,arraySums.getOrDefault(sum,0)+1);
+        }
+        return result;
+        /*
+           // Time Limit Exceeded Solution
         int count=0;
        for(int i=0;i<nums.length;i++){
            int sum=0;
@@ -10,5 +26,6 @@ class Solution {
            }
        } 
         return count;
+   */
     }
 }
