f95976e6a6fa717cc3ad283de81e11574da299c3
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-14 14:48:27 +0300

    Time: 1702 ms (5.10%), Space: 72.5 MB (26.19%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -5,7 +5,7 @@ As per given constrant : k>=0 and values in array can be in between 0 to 1000.
 As we want subarrays whose product are strictly less than k.
 so if(k<=1) return 0.
 and luckily here you dont have the problem that product of number is exceeding the Integer.Max Range i.e k is up to 10^6.
-   */
+   
     if(k<=1) return 0;
     int count = 0;
     int product = 1;
@@ -22,6 +22,23 @@ and luckily here you dont have the problem that product of number is exceeding t
         count += (right-left)+1; // basically it is used to find the length of the current window
          right++;
     }
-       return count;
+ return count;
+*/
+    
+// brute force solution
+    int count =0;
+    for(int i=0;i<nums.length;i++){
+        int product =1;
+        for (int j=i;j<nums.length;j++){
+            product *= nums[j];
+            if (product < k){
+                count++;
+            }else{
+                break;
+            }
+        }
+    }
+     return count;
+
 }
 }
