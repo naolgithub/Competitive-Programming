809e0d84f8fcd609c77924cbfd50ab92f2aaa9c3
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-14 21:36:06 +0300

    Time: 1 ms (100.00%), Space: 49.9 MB (90.77%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+       int windowStart=0;
+        int windowSum=0;
+        int minLengthSoFar=Integer.MAX_VALUE;
+        for(int windowEnd=0;windowEnd<nums.length;windowEnd++){
+            windowSum+=nums[windowEnd];
+            while(windowSum>=target){
+              int currentLength=windowEnd-windowStart+1;
+                minLengthSoFar=Math.min(minLengthSoFar,currentLength);
+                windowSum-=nums[windowStart];
+                windowStart++;
+            }
+        }
+        return minLengthSoFar==Integer.MAX_VALUE ? 0 : minLengthSoFar;
+    }
+}
