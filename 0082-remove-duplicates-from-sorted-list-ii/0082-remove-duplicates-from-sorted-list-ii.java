82e9dfa5a0d4285a98c071301ffd6e80fdaa0ee1
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-17 12:43:00 +0300

    Time: 2 ms (17.65%), Space: 44.2 MB (17.33%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -18,10 +18,10 @@ class Solution {
         // before the sublist of duplicates
         ListNode pred = sentinel;
         
-        while (head != null && head.next!=null) {
+        while (head != null) {
             // if it's a beginning of duplicates sublist 
             // skip all duplicates
-            if (head.val == head.next.val) {
+            if (head.next != null && head.val == head.next.val) {
                 // move till the end of duplicates sublist
                 while (head.next != null && head.val == head.next.val) {
                     head = head.next;    
