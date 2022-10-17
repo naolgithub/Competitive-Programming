8f486474f0c4451a71907ae66e849d06b1237e7b
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-17 12:40:06 +0300

    Time: 1 ms (85.38%), Space: 43.6 MB (60.28%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -9,21 +9,33 @@
  * }
  */
 class Solution {
+    // from solutions for the sake of COMMENT
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode dummyNaol=new ListNode(0,head);
-        ListNode temp=dummyNaol;
-        while(head!=null && head.next!=null){
-            if(head.val==head.next.val){
-               while(head.next!=null && head.val==head.next.val){
-                   head=head.next;
-               } 
-                temp.next=head.next;
+        // sentinel
+        ListNode sentinel = new ListNode(0, head);
+
+        // predecessor = the last node 
+        // before the sublist of duplicates
+        ListNode pred = sentinel;
+        
+        while (head != null && head.next!=null) {
+            // if it's a beginning of duplicates sublist 
+            // skip all duplicates
+            if (head.val == head.next.val) {
+                // move till the end of duplicates sublist
+                while (head.next != null && head.val == head.next.val) {
+                    head = head.next;    
+                }
+                // skip all duplicates
+                pred.next = head.next;     
+            // otherwise, move predecessor
+            } else {
+                pred = pred.next;    
             }
-            else{
-                temp=temp.next;
-            }
-            head=head.next;
-        }
-        return dummyNaol.next;
+                
+            // move forward
+            head = head.next;    
+        }  
+        return sentinel.next;
     }
 }
