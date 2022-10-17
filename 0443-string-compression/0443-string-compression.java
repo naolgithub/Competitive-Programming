3c2579dc650efd4e8d3b09d22b6924420cb31461
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-17 18:10:14 +0300

    Time: 4 ms (37.41%), Space: 43.4 MB (79.88%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int compress(char[] chars) {
+     StringBuilder sb=new StringBuilder();
+        sb.append(chars[0]);
+        int count =1;
+        for(int i=1;i<chars.length;i++){
+            char currentCharacter=chars[i];
+            char previousCharacter=chars[i-1];
+            if(currentCharacter==previousCharacter){
+                count++;
+            }
+            else if(currentCharacter!=previousCharacter){
+                if(count>1) {
+                    sb.append(count);
+                }
+                sb.append(currentCharacter);
+                count=1;
+            }
+        }
+        if(count>1){
+            sb.append(count);
+        }
+        for(int i=0;i<sb.length();i++){
+            chars[i]=sb.charAt(i);
+        }
+        return sb.length();
+    }
+}
