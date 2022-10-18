27b523a1eb68fb7671f5b713a3af48ea65e88499
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-18 15:44:45 +0300

    Time: 4 ms (51.70%), Space: 54.9 MB (71.28%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -11,22 +11,59 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        Stack<ListNode> stackOne=new Stack();
-        Stack<ListNode> stackTwo=new Stack();
-        while(headA!=null){
-            stackOne.push(headA);
-            headA=headA.next;
+         // solving Using hashset
+        /**
+        HashSet set = new HashSet<>();
+        
+        while(headA != null || headB != null)
+        {
+            if(set.contains(headA))
+            {
+                return headA;
+            }
+            else if(set.contains(headB))
+            {
+                return headB;
+            }
+            else if(headA == headB)
+            {
+                return headA;
+            }
+            
+            if(headA != null)
+            {
+                set.add(headA);
+                headA = headA.next;
+            }
+            if(headB != null)
+            {
+                set.add(headB);
+                headB = headB.next;
+            }
         }
-        while(headB!=null){
-            stackTwo.push(headB);
-            headB=headB.next;
+        return null;
+        
+        */
+            
+        //Solving Using stack
+        Stack<ListNode> stackA= new Stack<>();
+        Stack<ListNode> stackB= new Stack<>();
+        ListNode tempA=headA;
+        ListNode tempB=headB;
+        while(tempA!=null){
+            stackA.push(tempA);
+            tempA=tempA.next;
+        }
+        while(tempB!=null){
+            stackB.push(tempB);
+            tempB=tempB.next;
         }
         ListNode prev=null;
-        while(!stackOne.isEmpty() && !stackTwo.isEmpty()){
-            if(stackOne.peek()==stackTwo.peek()){
-                prev=stackOne.peek();
-                stackOne.pop();
-                stackTwo.pop();
+        while(!stackA.isEmpty()&&!stackB.isEmpty()){
+            if(stackA.peek()==stackB.peek()){
+                prev=stackB.peek();
+                stackA.pop();
+                stackB.pop();
             }
             else{
                 break;
