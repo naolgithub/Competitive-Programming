8d5b1f137afd1ad4cb926e173c758dad4265ddc0
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-19 20:15:56 +0300

    Time: 1 ms (78.54%), Space: 42.3 MB (66.63%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -9,6 +9,7 @@
  * }
  */
 class Solution {
+        /*
         private ListNode anotherHead;
         private ListNode tail;
         private void buildListNode(int data){
@@ -42,5 +43,45 @@ class Solution {
                     buildListNode(list.get(i));
             }
             return anotherHead;
+            */
+    public ListNode partition(ListNode head, int x) { 
+    /*
+    Time Complexity: O(N), where N is the number of nodes in the original linked list and we iterate the original list.
+Space Complexity: O(1), we have not utilized any extra space, the point to note is that we are reforming the original list, by moving the original nodes, we have not used any extra space as such.
+    */
+                // before and after are the two pointers used to create the two list
+        // before_head and after_head are used to save the heads of the two lists.
+        // All of these are initialized with the dummy nodes created.
+        ListNode before_head = new ListNode(0);
+        ListNode before = before_head;
+        ListNode after_head = new ListNode(0);
+        ListNode after = after_head;
+
+        while (head != null) {
+
+            // If the original list node is lesser than the given x,
+            // assign it to the before list.
+            if (head.val < x) {
+                before.next = head;
+                before = before.next;
+            } else {
+                // If the original list node is greater or equal to the given x,
+                // assign it to the after list.
+                after.next = head;
+                after = after.next;
+            }
+
+            // move ahead in the original list
+            head = head.next;
+        }
+
+        // Last node of "after" list would also be ending node of the reformed list
+        after.next = null;
+
+        // Once all the nodes are correctly assigned to the two lists,
+        // combine them to form a single list which would be returned.
+        before.next = after_head.next;
+
+        return before_head.next;
     }
 }
