22878c03d89a87c079a2d35b62d82128a83a8207
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-21 12:32:58 +0300

    Time: 2 ms (53.29%), Space: 41 MB (84.61%) - LeetHub

---
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -0,0 +1,20 @@
+class Solution {
+    /*
+    Maintain a sliding window of size k, keep updating the count of W out of it and find the minimum.
+
+Note: leftBlockBound and rightBlockBound are the lower and upper bounds of the sliding window.
+*/
+    public int minimumRecolors(String blocks, int k) {
+       int min = Integer.MAX_VALUE;
+        for (int lo = -1, hi = 0, white = 0; hi < blocks.length(); ++hi) {
+            white += blocks.charAt(hi) == 'W' ? 1 : 0;
+            if (hi - lo >= k) { // the window reaches size of k.
+                min = Math.min(min, white); // update  minimum.
+                // slide 1 step right the lower bound of the sliding
+                // window and update the value of white count.
+                white -= blocks.charAt(++lo) == 'W' ? 1 : 0;
+            }
+        }
+        return min;
+    }
+}
