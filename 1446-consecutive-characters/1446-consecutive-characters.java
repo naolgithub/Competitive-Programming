b00be40974ad7b6629242de7e98230dd6ae6549d
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-21 15:50:53 +0300

    Time: 2 ms (85.88%), Space: 42.7 MB (46.03%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxPower(String s) {
+        /*
+        In other words, we need to find the Longest Substring with the same characters.
+        */
+        int count = 0;
+        int maxCount = Integer.MIN_VALUE;
+        char previousCharacter = ' ';
+        for (int i = 0; i < s.length(); i++) {
+            char currentCharacter = s.charAt(i);
+            if (currentCharacter == previousCharacter) {
+     // if currentCharacter is same as previousCharacter, increase the count by one
+                count++;
+            } 
+            else {
+                // else, reset the count to one and assign currentCharacter to previousCharacter
+                count = 1;
+                previousCharacter = currentCharacter;
+            }
+            maxCount = Math.max(maxCount, count);
+        }
+        return maxCount;
+    }
+}
