51e27db90052bbb235f3c5672cc01f1d949e793e
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-22 22:23:23 +0300

    Time: 25 ms (44.07%), Space: 46.8 MB (77.61%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-//using loop iteration
+      //using loop iteration
         if (n < 1) {
             return false;
         }
-
         while (n % 3 == 0) {
             n /= 3;
         }
-
         return n == 1;       
     }
 }
