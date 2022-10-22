b193000cae0e9e9b1684de7e9013e708a1c70e90
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-10-22 23:19:48 +0300

    Time: 12 ms (57.29%), Space: 51.6 MB (43.57%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.java
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.java
@@ -1,5 +1,18 @@
 class Solution {
     public int findMaxK(int[] nums) {
+        //using hashing(hash table)
+        Set<Integer> set= new HashSet();
+        for(int num:nums) set.add(num);
+        //since we return -1 if there is no largestPositiveNegative Integer,we assign -1 to largestPosNeg.
+        int largestPosNeg=-1;
+        for(int i:nums){
+            if(i>0 &&set.contains(-i)){
+               largestPosNeg=Math.max(largestPosNeg,i); 
+            }
+        }
+        return largestPosNeg;
+        /*
+        //using nested loops and sorting
        Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
             for (int j = nums.length-1; j>=0; j--) {
@@ -7,5 +20,6 @@ class Solution {
             }
         }
         return -1;
+        */
     }
 }
