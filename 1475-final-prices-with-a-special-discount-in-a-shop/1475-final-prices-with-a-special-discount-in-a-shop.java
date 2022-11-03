9a95130a58201c110abf7c6882422b36c93e2d74
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-11-03 11:07:26 +0300

    Time: 4 ms (67.85%), Space: 42.5 MB (87.52%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,6 +1,18 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
-        //using Stack
+      /*
+        for(int i=0;i<prices.length;i++){
+            if(i+1==prices.length) break;
+            for(int j=i+1;j<prices.length;j++){
+                if(prices[j]<=prices[i]){
+                    prices[i]=prices[i]-prices[j];
+                    break;
+                }
+            }
+        }
+        return prices;
+      */
+         //using Stack
      Stack<Integer> stack = new Stack<>();
         for (int i=prices.length-1; i>=0; i--) {
             //if peek of the stack is greater than price which is going to be added, we remove peek of stack
