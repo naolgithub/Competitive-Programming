3c4f2df9341f21aecfc103f33f49afcfc9abaf9e
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-11-08 01:14:28 +0300

    Time: 7 ms (83.08%), Space: 44.7 MB (33.86%) - LeetHub

---
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -0,0 +1,36 @@
+class Solution {
+    //https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/discuss/535421/Java-Clean-HashMap-solution-with-explanation
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        /*
+        The putIfAbsent(K key, V value) method of HashMap class is used to map the specified key with the specified value, only if no such key exists (or is mapped to null)  in this HashMap instance.
+        */
+        //1.Create a copy of the input array. copy = [8,1,2,2,3]
+        //2.Sort the copy array. copy = [1,2,2,3,8]
+        /*
+        3.Fill the map: number => count (where count is an index in sorted array, so first number with index 0 has 0 numbers less than it, index 1 has 1 number less, etc). We update only first time we enocunter the number so that way we skip duplicates.
+map[1]=>0
+map[2]=>1
+map[3]=>3
+map[8]=>4
+        */
+        /*
+        4.We re-use our copy array to get our result, we iterate over original array, and get counts from the map.
+[4,0,1,1,3]
+        */
+       Map<Integer, Integer> map = new HashMap<>();
+        int[] copy = nums.clone();
+        
+        Arrays.sort(copy);
+        
+        for (int i = 0; i < nums.length; i++) {
+            map.putIfAbsent(copy[i], i);
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            copy[i] = map.get(nums[i]);
+        }
+        
+        return copy;
+ 
+    }
+}
