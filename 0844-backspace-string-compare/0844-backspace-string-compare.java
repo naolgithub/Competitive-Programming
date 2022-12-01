03f09aeab5756b446e127b513fd66b629cb86dcd
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-12-01 10:22:36 +0300

    Time: 2 ms (77.26%), Space: 40.7 MB (83.30%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,5 +1,10 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
+        /*
+        Using stack
+        Time Complexity: O(N)
+        Space Complexity:O(N)
+        */
         Stack<Character> stackS=new Stack();
         Stack<Character> stackT=new Stack();
         for(int i=0;i<s.length();i++){
@@ -19,12 +24,15 @@ class Solution {
                 if(!stackT.isEmpty()) stackT.pop();
             }
         }
+        //checking if size of stackS and stackT are equal or not
         if (stackS.size() != stackT.size()) return false;
+        //while removing the top characters checking whether the top characters of stackS and stackT are equal or not
         while (!stackS.isEmpty() && !stackT.isEmpty()) {
             if (stackS.pop() != stackT.pop()) {
                 return false;
             }
         }
+        // just finally return true
         return true;
     }
 }
