f93820be7fbe5d8e94339f579e1c8b1b1c26a073
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-12-08 11:16:09 +0300

    Time: 7 ms (85.69%), Space: 43.2 MB (79.84%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,45 +1,49 @@
 class Solution {
+    // Define the character range
+    private static final int CHAR_RANGE = 128;
     public int lengthOfLongestSubstring(String s) {
-       /*
-        int i = 0;
-         int j = 0;
-         int res = 0;
-          HashSet<Character> set = new HashSet<>();
-        while (j < s.length()) {
-            if (!set.contains(s.charAt(j))) {
-                 set.add(s.charAt(j));
-                 j++;
-                 res = Math.max(res, j - i);
-             } else {
-                 set.remove(s.charAt(i));
-                 i++;
-             }
+         // base case
+        if (s == null || s.length() == 0) {
+            return 0;
         }
-         return res;
-        */
-        // it is a sliding window question
-        // lets take two pointers and hashSet 
-        // since hashset only stores unique data
-        int a_pointer=0;
-        int b_pointer=0;
-        int longestSubstring=0;
-        // we are gonna use HashSet since it stores only unique data
-        HashSet<Character> hashSet=new HashSet<>();
-        while(b_pointer<s.length()){
-            //adding elements into hashSet
-            if(!hashSet.contains(s.charAt(b_pointer))){
-                hashSet.add(s.charAt(b_pointer));
-                b_pointer++;
-        //finding the maximum substring length
-        longestSubstring=Math.max(longestSubstring,hashSet.size());
+ 
+        // boolean array to mark characters present in the current window
+        boolean[] window = new boolean[CHAR_RANGE];
+ 
+        // stores the longest substring boundaries
+        int begin = 0;
+        int end = 0;
+        // `[low…high]` maintain the sliding window boundaries
+        for (int low = 0, high = 0; high < s.length(); high++)
+        {
+            // if the current character is present in the current window
+            if (window[s.charAt(high)])
+            {
+                // remove characters from the left of the window till
+                // we encounter the current character
+                while (s.charAt(low) != s.charAt(high))
+                {
+                    window[s.charAt(low)] = false;
+                    low++;
+                }
+ 
+                low++;        // remove the current character
             }
-                // if you encounter any previous element, remove the previously added element not the new encountered one.
-            else if(hashSet.contains(s.charAt(a_pointer))){
-                  hashSet.remove(s.charAt(a_pointer));
-                  a_pointer++;   
-                 }
-            
+            else {
+                // if the current character is not present in the current
+                // window, include it
+                window[s.charAt(high)] = true;
+ 
+                // update the maximum window size if necessary
+                if (end - begin < high - low)
+                {
+                    begin = low;
+                    end = high;
+                }
+            }
         }
-        return longestSubstring;
+ 
+        // return the longest substring found at `str[begin…end]`
+        return s.substring(begin, end + 1).length();
     }
 }
