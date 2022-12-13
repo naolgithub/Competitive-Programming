9c28b6eb137c536f82cc029619d6fbf7ce6f53cc
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-12-13 23:42:09 +0300

    Time: 0 ms (100.00%), Space: 41.9 MB (85.63%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max=0;
+        for(int row=0;row<accounts.length;row++){
+            int sum=0;
+            for(int column=0;column<accounts[row].length;column++){
+                sum+=accounts[row][column];
+                max=Math.max(max,sum);
+            }
+        }
+        return max;
+    }
+}
