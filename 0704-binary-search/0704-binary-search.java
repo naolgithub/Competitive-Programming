e5379d6db6c4d1220ec5c0b37b95b715f242c264
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-12-14 17:01:24 +0300

    Time: 0 ms (100.00%), Space: 43 MB (88.90%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -15,5 +15,22 @@ class Solution {
         else{
             return binarySearch(array,target,start,middle-1);
         }
+        /*
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right){
+            int middle=left+(right-left)/2;
+            if(nums[middle]==target){
+             return middle;
+            }
+            else if(nums[middle]>target){
+                 right=middle-1;
+            }
+            else{
+                left=middle+1;
+            }
+        }
+        return -1;
+        */
     }
 }
