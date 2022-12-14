54aac78d94938129543cb5bd73de81c32cd46a3c
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-12-14 16:56:36 +0300

    Time: 0 ms (100.00%), Space: 43.1 MB (82.98%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int start=0;
+        int end=nums.length-1;
+        return binarySearch(nums,target,start,end);
+    }
+    private int binarySearch(int[] array,int target,int start,int end){
+        if(start>end) return -1;
+        int middle=start+(end-start)/2;
+        if(target==array[middle]) return middle;
+        else if(target>array[middle]){
+            return binarySearch(array,target,middle+1,end);
+        }
+        else{
+            return binarySearch(array,target,start,middle-1);
+        }
+    }
+}
