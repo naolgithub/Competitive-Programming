1c869e9efbdab808af15b526e2bdb5e5c8d7acae
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-12-31 20:13:08 +0300

    Time: 0 ms (100.00%), Space: 41.9 MB (97.63%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.java
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -8,60 +8,46 @@
  */
  
 class Solution {
-    public int findInMountainArray(int target, MountainArray mountainArr) {
-        
-        int peak = peakIndex(mountainArr);
-        int leftHalf = binarySearch(target, mountainArr, 0, peak);
-        if(leftHalf!=-1){
-            return leftHalf;
-        }else{
-            return binarySearch(target,mountainArr, peak+1, mountainArr.length()-1);
-        }
-        
-    }
-    
-    public int peakIndex(MountainArray mountainArr){
-        int start =0;
-        int end = mountainArr.length()-1;
-            
-        while(start<end){
-            int mid = start + (end-start)/2;
-            
-            if(mountainArr.get(mid)>mountainArr.get(mid+1)){
-                end = mid;
-            }else{
-                start = mid+1;
-            }
-        }
-        return start;
-    }
-    
-    public int binarySearch(int target,MountainArray mountainArr,int start, int end){
-        
-        boolean isAsc = mountainArr.get(start) < mountainArr.get(end);
-        
+    private int OrderAgnosticBinarySearch(int target,MountainArray mountainArr,int start,int end){
+        boolean isAscending=mountainArr.get(start)<mountainArr.get(end);
         while(start<=end){
-            
-            int mid = start + (end-start)/2;
-            
-            if(target == mountainArr.get(mid)){
-                return mid;
-            }
-            if(isAsc){
-                if(target < mountainArr.get(mid)){
-                    end=mid-1;
-                }else{
-                    start=mid+1;
-                }
+            int mid=start+(end-start)/2;
+            if(mountainArr.get(mid)==target) return mid;
+            if(isAscending){
+            if(mountainArr.get(mid)>target) end=mid-1;
+            else start=mid+1;
             }
             else{
-                if(target > mountainArr.get(mid)){
-                    end=mid-1;
-                }else{
-                    start=mid+1;
-                }
+                if(mountainArr.get(mid)<target) end=mid-1;
+                else start=mid+1;
             }
         }
         return -1;
+    }
+    private int findPeakOfMountain(MountainArray mountainArr){
+        //at the final both start and end points to the same element
+        //when we find the peak of the mountain.
+        int start=0;
+        int end=mountainArr.length()-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(mountainArr.get(mid)>mountainArr.get(mid+1)){
+                end=mid;
+            }
+            else start=mid+1;
+        }
+        return start;
+    }
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        //finding peak of the mountain.
+        int peakOfMountain=findPeakOfMountain(mountainArr);
+        
+        //if our target is  on the left side of the peak
+            int leftSideTarget=OrderAgnosticBinarySearch(target,mountainArr,0,peakOfMountain);
+        if(leftSideTarget!=-1) return leftSideTarget;
+        
+        //if our target is on the right side of the peak
+            else return OrderAgnosticBinarySearch(target,mountainArr,peakOfMountain+1,mountainArr.length()-1);
+       
     }
 }
