6c5c7a285cb2ac580a5c90a35dd753023a8515c0
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2022-12-31 12:08:03 +0300

    Time: 17 ms (86.64%), Space: 39.1 MB (77.89%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -0,0 +1,19 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int firstBadProduct=0;
+        int start=1;
+        int end=n;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(isBadVersion(mid)){
+                firstBadProduct=mid;
+                end=mid-1;
+            }
+            else start=mid+1;
+        }
+        return firstBadProduct;
+    }
+}
