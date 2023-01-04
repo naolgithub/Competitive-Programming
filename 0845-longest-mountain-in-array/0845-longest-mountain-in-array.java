fa54d654f51487db2696817bd1a3f5954c9fb935
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2023-01-04 11:37:24 +0300

    Time: 4 ms (57.87%), Space: 51.9 MB (41.70%) - LeetHub

---
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int longestMountain(int[] arr) {
+        int longestmountain=0;
+        //peak of the mountain cant be at the zeroth index.
+        //peak point should have a lesser neighbor points.
+        //first and last elements of an array cant be a peak point because peak of the mountain should have a lesser value neighbor points.
+        for(int i=1;i<=arr.length-2;){
+            //looking for neighbor of an element
+            if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+                //count must begin from one because we start counting from the peak of the mountain.
+                int count=1;
+                int j=i;
+                while(j>0 && arr[j]>arr[j-1]){
+                    count++;
+                    j--;
+                }
+                while(i<arr.length-1 && arr[i]>arr[i+1]){
+                    i++;
+                    count++;
+                }
+                longestmountain=Math.max(longestmountain,count);
+            }
+            else i++;
+        }
+        return longestmountain;
+    }
+}
