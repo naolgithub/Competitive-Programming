3df577bc38b240961a7cb07a52bca192307b837a
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2023-01-05 14:31:44 +0300

    Time: 8 ms (46.50%), Space: 54.4 MB (55.19%) - LeetHub

---
+++ 0986-interval-list-intersections/0986-interval-list-intersections.java
@@ -0,0 +1,21 @@
+class Solution {
+ public int[][] intervalIntersection(int[][] A, int[][] B) {
+    List<int[]> list = new ArrayList();
+    int aPointer = 0;
+    int bPointer=0;
+    while( aPointer < A.length && bPointer < B.length){
+        int low = Math.max( A[aPointer][0], B[bPointer][0]);
+        int high = Math.min(A[aPointer][1],B[bPointer][1]);
+        if( low <= high){
+            list.add( new int[]{low,high});
+        }
+        if( A[aPointer][1] < B[bPointer][1])
+            aPointer++;
+        else
+            bPointer++;
+            
+    }
+   
+    return list.toArray(new int[list.size()][]);
+}
+}
