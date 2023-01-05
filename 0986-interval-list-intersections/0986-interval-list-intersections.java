56aadbd76f576738b8eb26c495c9b051cf3e7e73
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2023-01-05 14:42:42 +0300

    Time: 3 ms (93.94%), Space: 43.2 MB (71.44%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.java
+++ 0986-interval-list-intersections/0986-interval-list-intersections.java
@@ -4,6 +4,8 @@ class Solution {
     int aPointer = 0;
     int bPointer=0;
     while( aPointer < A.length && bPointer < B.length){
+        //low is the startPoint of intersection
+        //high is the endPoint of intersection
         int low = Math.max( A[aPointer][0], B[bPointer][0]);
         int high = Math.min(A[aPointer][1],B[bPointer][1]);
         if( low <= high){
