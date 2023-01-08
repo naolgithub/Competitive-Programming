ab80adcdd1129e8f718515d64215ba86bbf76431
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2023-01-08 10:50:25 +0300

    Time: 17 ms (72.30%), Space: 41.8 MB (88.97%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int threeSumClosest=nums[0]+nums[1]+nums[nums.length-1];
+        for(int i=0;i<nums.length-2;i++){
+            int start=i+1;
+            int end=nums.length-1;
+            while(start<end){
+                int currentSum=nums[i]+nums[start]+nums[end];
+                if(currentSum>target) end--;
+                else start++;
+                if(Math.abs(currentSum-target)<Math.abs(threeSumClosest-target)){
+                    threeSumClosest=currentSum;
+                }
+            }
+        }
+        return threeSumClosest;
+    }
+}
