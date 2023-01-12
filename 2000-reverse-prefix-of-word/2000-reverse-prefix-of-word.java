c32731dde04318bfcd4958898ea5ca624c62c3c8
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2023-01-12 16:56:23 +0300

    Time: 0 ms (100.00%), Space: 40.5 MB (84.06%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -1,7 +1,8 @@
 class Solution {
     public String reversePrefix(String word, char ch) {
+        // converting string to characters
         char[] character=word.toCharArray();
-    
+        //reversing the segment of the word that starts at index 0 and ends at the index of the ch.
             int start=0;
             int end=word.indexOf(ch);
             while(start<end){
@@ -11,7 +12,7 @@ class Solution {
                 start++;
                 end--;
             }
-        
+        //building a string from the reversed characters
         StringBuilder sb=new StringBuilder();
         for(int i=0;i<character.length;i++){
             sb.append(character[i]);
