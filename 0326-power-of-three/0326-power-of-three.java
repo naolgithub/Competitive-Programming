8736815b39be9df79cadc988e1bd2f233f6f11c7
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2023-01-15 22:56:27 +0300

    Time: 16 ms (63.38%), Space: 42.2 MB (60.42%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,5 +1,12 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
+        //1.Using loop
+        /*
+        if(n<1) return false;
+        while(n%3==0) n/=3;
+        return n==1;
+        */
+        //2.Using recursion
         if(n<1) return false;
         if(n%3==0) return isPowerOfThree(n/3);
         return n==1;
