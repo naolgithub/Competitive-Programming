0340d8aff620b23ebd27888e34ea35b00d6c3202
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2023-01-15 21:51:58 +0300

    Time: 1 ms (100.00%), Space: 39.3 MB (88.95%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,10 +1,5 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        //using built-in Math function
-        /*
-        x=log(n)/log(4)
-        check n==4^x
-        */
         if(n==0) return false;
         int x=(int)(Math.log(n)/Math.log(4));
         return n==(int)Math.pow(4,x);
