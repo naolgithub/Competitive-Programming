c03871799cdc92824337b6b5b4bb06e51dc715a8
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2023-01-15 22:46:26 +0300

    Time: 1 ms (100.00%), Space: 39.5 MB (81.97%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,6 +1,13 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
+        //1.Using recursion
+        /*
+        if(n<1) return false;
+        if(n%2==0) return isPowerOfTwo(n/2);
+        return n==1;
+        */
         
+        //2.Using math approach
         if(n==0) return false;
         int x=(int)(Math.log(n)/Math.log(2));
         return n==(int)Math.pow(2,x);
