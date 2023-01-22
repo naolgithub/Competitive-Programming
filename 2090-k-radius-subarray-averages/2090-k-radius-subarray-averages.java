a6a43d8effcc9de40377312f26710dbbbffaaedf
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2023-01-22 22:15:16 +0300

    Time: 13 ms (38.98%), Space: 61.9 MB (75.20%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -26,12 +26,13 @@ class Solution {
     public int[] getAverages(int[] nums, int k) {
         int[] result = new int[nums.length];
         long currentSum = 0;
+        int windowStart=0;
         for (int windowEnd = 0; windowEnd < nums.length; windowEnd++) {
             if (windowEnd < k) result[windowEnd] = -1;            
             currentSum += nums[windowEnd];
             if (windowEnd >= 2*k) {
                 result[windowEnd - k] = (int)(currentSum / (2*k + 1));
-                currentSum -= nums[windowEnd - 2*k];
+                currentSum -= nums[windowStart++];
             }
 			if (windowEnd >= nums.length - k) result[windowEnd] = -1;
         }
