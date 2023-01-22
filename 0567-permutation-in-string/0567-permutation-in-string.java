d1b56c6c091e63331607817c5bcb0101fb53b209
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2023-01-22 15:04:32 +0300

    Time: 1002 ms (12.25%), Space: 42.6 MB (51.33%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -5,6 +5,11 @@ class Solution {
         return new String(arrayOfCharacters);
     }
     public boolean checkInclusion(String s1, String s2) {
+        /*
+        let l1 be the length of string s1 and s2 be the length of string s2
+        Time complexity:O(l1Log(l1) + (l2-l1)l1Log(l1))
+        Space complexity:O(1)
+        */
         //points noticed: 1 sorting of string using Arrays.sort() method after changing string to array of character.
         String sortedSOne=sort(s1);
         for(int i=0;i<=s2.length()-s1.length();i++){
