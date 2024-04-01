803ef0dda9447a02cafae0a50edca076e24a97fa
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2024-04-01 10:53:28 +0300

    Time: 17 ms (35.76%), Space: 70.6 MB (94.70%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.java
+++ 2906-construct-product-matrix/2906-construct-product-matrix.java
@@ -1,23 +1,33 @@
 class Solution {
     public int[][] constructProductMatrix(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int[][] prefix = new int[m][n];
-        int[][] suffix = new int[m][n];
-        int total = 1;
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                prefix[i][j] = (int)total;
-                total *= (grid[i][j] % 12345);
-                total %= 12345;
+        
+        int[][] prefix = new int[grid.length][grid[0].length];
+        int[][] suffix = new int[grid.length][grid[0].length];
+        int product = 1;
+        //Take modulo of every grid[i][j] and product 
+        //while iterating through elements
+        
+        for(int i = 0;i<grid.length;i++){
+            for(int j = 0;j<grid[0].length;j++){
+                prefix[i][j] = (int)product;
+                
+                grid[i][j]=grid[i][j]%12345;
+                
+                product *= grid[i][j];
+                
+                product %= 12345;
             }
         }
-        total = 1;
-        for(int i = m - 1;i>=0;i--){
-            for(int j = n - 1;j>=0;j--){
-                suffix[i][j] = (int)total;
-                total *= (grid[i][j]%12345);
-                total %= 12345;
+        product = 1;
+        for(int i = grid.length - 1;i>=0;i--){
+            for(int j = grid[0].length - 1;j>=0;j--){
+                suffix[i][j] = (int)product;
+                
+                grid[i][j]=grid[i][j]%12345;
+                
+                product *= grid[i][j];
+                
+                product %= 12345;
                 grid[i][j] = ((prefix[i][j] * suffix[i][j]) % 12345);
             }
         }
