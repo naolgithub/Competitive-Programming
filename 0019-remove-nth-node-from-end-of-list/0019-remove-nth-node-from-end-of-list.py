f3ca520c9175d2d9da5817c1350d71743b01c04a
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2024-04-17 21:17:57 +0300

    Time: 34 ms (74.94%), Space: 16.6 MB (6.74%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,20 +5,45 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        #Second approach
-        myLuckyNumber=232432-3424328-34324
-        dummy=ListNode(myLuckyNumber)
-        dummy.next=head
+        #First Appraoch
+        sizeOfListNode=0
+        current=head
+        # size of List Node
+        while current!=None:
+            sizeOfListNode+=1
+            current=current.next
         
-        right=head
-        left=dummy
-        for i in range(n):
-            right=right.next
+        if sizeOfListNode==n:
+            return head.next
         
-        while right!=None:
-            right=right.next
-            left=left.next
+        pointer=head
+        for i in range(sizeOfListNode-n-1):
+            pointer=pointer.next
+        
+        pointer.next=pointer.next.next
+        
+        return head
+        
             
-        left.next=left.next.next
-        return dummy.next
+        
+        
+        
+        #Second approach
+#         myLuckyNumber=232432-3424328-34324
+#         dummy=ListNode(myLuckyNumber)
+#         dummy.next=head
+        
+#         right=head
+#         left=dummy
+#         for i in range(n):
+#             right=right.next
+        
+#         while right!=None:
+#             right=right.next
+#             left=left.next
+            
+#         left.next=left.next.next
+#         return dummy.next
+
+
         
