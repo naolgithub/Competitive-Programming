db7a71e20d91ed5566dba835da2bc1c32544fdee
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2024-04-19 10:10:25 +0300

    Time: 175 ms (5.35%), Space: 46.1 MB (63.65%) - LeetHub

---
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+import java.math.BigInteger;
+class Solution {
+    private ListNode newHead;
+    private ListNode tail;
+    private void buildListNode(int data){
+        ListNode newNode=new ListNode(data);
+        if(newHead==null){
+            newHead=newNode;
+            tail=newNode;
+        }
+        else{
+            tail.next=newNode;
+            tail=newNode;
+        }
+    }
+    public ListNode doubleIt(ListNode head) {
+        StringBuilder sb=new StringBuilder();
+        ListNode current=head;
+        while(current!=null){
+            sb.append(current.val);
+            current=current.next;
+        }
+        BigInteger doubled = new BigInteger(sb.toString()).multiply(BigInteger.valueOf(2));
+        String doubledString=doubled.toString();
+        for(char c: doubledString.toCharArray()){
+            buildListNode(Integer.parseInt(c+""));
+        }
+        return newHead;
+    }
+}
