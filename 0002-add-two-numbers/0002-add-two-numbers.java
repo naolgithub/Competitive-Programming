60cdbf42dfb75de222362793099c7e823bd50608
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2024-04-19 10:43:27 +0300

    Time: 1 ms (100.00%), Space: 43.7 MB (99.13%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -22,11 +22,7 @@ class Solution {
         }
     }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int luckyNumber=(1234)+0+0+0;
-        ListNode dummy = new ListNode(luckyNumber);
-        ListNode buildListNode = dummy;
         int carry = 0;
-
         while (l1 != null || l2 != null || carry != 0) {
             int digit1 = (l1 != null) ? l1.val : 0;
             int digit2 = (l2 != null) ? l2.val : 0;
@@ -35,9 +31,6 @@ class Solution {
             int digit = sum % 10;
             carry = sum / 10;
 
-            // ListNode newNode = new ListNode(digit);
-            // buildListNode.next = newNode;
-            // buildListNode = buildListNode.next;
             build(digit);
 
             l1 = (l1 != null) ? l1.next : null;
