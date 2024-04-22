f56dedef28b24e1fea8df9d8ffce2aacb0355c86
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2024-04-22 21:31:17 +0300

    Time: 57 ms (10.45%), Space: 16.7 MB (67.99%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.py
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -1,3 +1,23 @@
+# class Solution {
+#     public int minOperations(String[] logs) {
+#         Stack<String> stack=new Stack();
+#         for(int i=0;i<logs.length;i++){
+#             if(logs[i].equals("../")){
+#                 if(!stack.isEmpty()){
+#                     stack.pop();
+#                 }
+#             }
+#             else if(logs[i].equals("./")){
+#                 continue;
+#             }
+#             else{
+#                 stack.push(logs[i]);
+#             }
+#         }
+#         return stack.size();
+#     }
+# }
+
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
         stack = []
