d6ad11fe9aeae3c5c156d8920733b506dfa474cf
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2024-05-18 16:05:50 +0300

    Time: 5 ms (100.00%), Space: 44.9 MB (44.46%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,17 +10,37 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        TreeNode currentNode=root;
-        while(currentNode!=null){
-            if(p.val>currentNode.val && q.val>currentNode.val){
-                currentNode=currentNode.right;
-            }
-            else if(p.val<currentNode.val && q.val<currentNode.val){
-                currentNode=currentNode.left;
-            }
-            else{
-                return currentNode;
-            }
+        //First Approach
+        
+        // TreeNode currentNode=root;
+        // while(currentNode!=null){
+        //     if(p.val>currentNode.val && q.val>currentNode.val){
+        //         currentNode=currentNode.right;
+        //     }
+        //     else if(p.val<currentNode.val && q.val<currentNode.val){
+        //         currentNode=currentNode.left;
+        //     }
+        //     else{
+        //         return currentNode;
+        //     }
+        // }
+        // return null;
+        
+        //Second Approach
+        if(Math.min(p.val,q.val)<root.val && Math.max(p.val,q.val)>root.val){
+            return root;
+        }
+        else if(p.val==root.val){
+            return root;
+        }
+        else if(q.val==root.val){
+            return root;
+        }
+        else if(p.val<root.val && q.val<root.val){
+            return lowestCommonAncestor(root.left,p,q);
+        }
+        else if(p.val>root.val && q.val>root.val){
+           return lowestCommonAncestor(root.right,p,q); 
         }
         return null;
     }
