aa24c662a98b22173878175c86c4f97fe62bf9b3
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2024-05-21 21:32:33 +0300

    Time: 46 ms (6.70%), Space: 17.2 MB (91.74%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.py
@@ -0,0 +1,44 @@
+# """
+# This is MountainArray's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class MountainArray:
+#    def get(self, index: int) -> int:
+#    def length(self) -> int:
+
+class Solution:
+    def findPeakOfMountain(self,mountainArr):
+        left=0
+        right=mountainArr.length()-1
+        while left<right:
+            middle=left+(right-left)//2
+            if mountainArr.get(middle)>mountainArr.get(middle+1):
+                right=middle
+            else:
+                left=middle+1
+        return left
+    def binarySearch(self,target,mountainArr,left,right):
+        isAscending=mountainArr.get(left)<mountainArr.get(right)
+        while left<=right:
+            middle=left+(right-left)//2
+            if mountainArr.get(middle)==target:
+                return middle
+            if isAscending:
+                if mountainArr.get(middle)>target:
+                    right=middle-1
+                else:
+                    left=middle+1
+            else:
+                if mountainArr.get(middle)<target:
+                    right=middle-1
+                else:
+                    left=middle+1
+        return -1
+    def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
+        peakOfMountain=self.findPeakOfMountain(mountain_arr)
+        leftSideTarget=self.binarySearch(target,mountain_arr,0,peakOfMountain)
+        if leftSideTarget!=-1:
+            return leftSideTarget
+        else:
+            return self.binarySearch(target,mountain_arr,peakOfMountain+1,mountain_arr.length()-1)
+        
