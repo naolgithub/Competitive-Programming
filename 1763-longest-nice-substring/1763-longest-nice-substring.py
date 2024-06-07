4404a529ac6ddca0ffd1a73ac2eefc42f28228c2
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2024-06-07 20:39:39 +0300

    Time: 300 ms (7.01%), Space: 16.5 MB (83.18%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.py
+++ 1763-longest-nice-substring/1763-longest-nice-substring.py
@@ -19,13 +19,13 @@ class Solution:
                     return False
             return True
         
-        result = ""
+        longestSubstring = ""
         n = len(s)
         for i in range(n):
             for j in range(i + 1, n + 1):
                 substring = s[i:j]
-                if isNice(substring) and len(substring) > len(result):
-                    result = substring
+                if isNice(substring) and len(substring) > len(longestSubstring):
+                    longestSubstring = substring
 
-        return result
+        return longestSubstring
 
