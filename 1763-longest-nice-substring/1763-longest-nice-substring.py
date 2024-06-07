007eb50ffab6e9b93ce1fce0bf0ac7f23187da1d
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2024-06-07 20:54:23 +0300

    Time: 303 ms (7.01%), Space: 16.4 MB (83.18%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.py
+++ 1763-longest-nice-substring/1763-longest-nice-substring.py
@@ -1,30 +1,29 @@
 class Solution:
+    def isNice(self,substring) -> bool:
+        lower = [False] * 26
+        upper = [False] * 26
+
+        for char in substring:
+            if char.islower():
+                lower[ord(char) - ord('a')] = True
+            elif char.isupper():
+                upper[ord(char) - ord('A')] = True
+
+        for i in range(26):
+            if lower[i] != upper[i]:
+                return False
+        return True
+    
     def longestNiceSubstring(self, s: str) -> str:
         if not s or len(s) < 2:
             return ""
 
-        def isNice(substring) -> bool:
-            lower = [False] * 26
-            upper = [False] * 26
-            
-            for char in substring:
-                
-                if char.islower():
-                    lower[ord(char) - ord('a')] = True
-                elif char.isupper():
-                    upper[ord(char) - ord('A')] = True
-                    
-            for i in range(26):
-                if lower[i] != upper[i]:
-                    return False
-            return True
-        
         longestSubstring = ""
         n = len(s)
         for i in range(n):
             for j in range(i + 1, n + 1):
                 substring = s[i:j]
-                if isNice(substring) and len(substring) > len(longestSubstring):
+                if self.isNice(substring) and len(substring) > len(longestSubstring):
                     longestSubstring = substring
 
         return longestSubstring
