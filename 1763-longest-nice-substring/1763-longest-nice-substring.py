8a43eb02d76bb223ba8bc2a8b4118460d33af902
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2024-06-07 20:58:09 +0300

    Time: 286 ms (7.95%), Space: 16.5 MB (48.83%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.py
+++ 1763-longest-nice-substring/1763-longest-nice-substring.py
@@ -22,9 +22,9 @@ class Solution:
         n = len(s)
         for i in range(n):
             for j in range(i + 1, n + 1):
-                substring = s[i:j]
-                if self.isNice(substring) and len(substring) > len(longestSubstring):
-                    longestSubstring = substring
+                currentSubstring = s[i:j]
+                if self.isNice(currentSubstring) and len(currentSubstring) > len(longestSubstring):
+                    longestSubstring = currentSubstring
 
         return longestSubstring
 
