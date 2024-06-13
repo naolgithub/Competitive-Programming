93210cfe1fa2be442ae60fef21b5c3ebf3c4e1b9
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2024-06-13 16:08:43 +0300

    Time: 3 ms (31.21%), Space: 42.5 MB (26.11%) - LeetHub

---
+++ 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.java
@@ -0,0 +1,35 @@
+import java.math.BigInteger;
+class Solution {
+    public boolean splitString(String s) {
+        // Iterate over possible lengths of the first substring
+        for (int i = 1; i < s.length(); i++) {
+            BigInteger firstValue = new BigInteger(s.substring(0, i));
+            if (canSplit(s, firstValue, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean canSplit(String s, BigInteger previousValue, int start) {
+        if (start == s.length()) {
+            return true;
+        }
+        
+        BigInteger currentValue = BigInteger.ZERO;
+        for (int i = start; i < s.length(); i++) {
+            currentValue = currentValue.multiply(BigInteger.TEN).add(BigInteger.valueOf(s.charAt(i) - '0'));
+            
+            if (currentValue.compareTo(previousValue) >= 0) {
+                return false;
+            }
+            
+            if (previousValue.subtract(currentValue).equals(BigInteger.ONE)) {
+                if (canSplit(s, currentValue, i + 1)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
