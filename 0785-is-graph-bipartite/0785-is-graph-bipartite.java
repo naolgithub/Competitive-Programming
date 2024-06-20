777778b13a72bf8b9ac6f66d2c4387d47958540f
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2024-06-20 21:34:53 +0300

    Time: 0 ms (100.00%), Space: 44.5 MB (90.07%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,27 +1,34 @@
 class Solution {
-    private boolean dfs(int[][] graph,int[] colors,int node, int color){
-        colors[node]=color;
-        for(int neighbor: graph[node]){
-            if(colors[neighbor]==0){
-                if(dfs(graph,colors,neighbor,-color)==false){
+    private boolean dfs(int[][] graph, int[] colors, int node, int color) {
+        colors[node] = color;
+
+        for (int neighbor : graph[node]) {
+            if (colors[neighbor] == 0) {
+                // If the neighbor is uncolored, color it with the opposite color
+                if (!dfs(graph, colors, neighbor, -color)) {
                     return false;
                 }
-            }
-            else if(colors[neighbor]==color){
+            } else if (colors[neighbor] == color) {
+                // If the neighbor is colored with the same color, the graph is not bipartite
                 return false;
             }
         }
+
         return true;
     }
     public boolean isBipartite(int[][] graph) {
-        int[] colors=new int[graph.length];
-        for(int i=0;i<graph.length;i++){
-            if(colors[i]==0){
-                if(dfs(graph,colors,i,1)==false){
+        int n = graph.length;
+        int[] colors = new int[n]; // 0: uncolored, 1: color1, -1: color2
+
+        for (int i = 0; i < n; i++) {
+            if (colors[i] == 0) {
+                // If the node is uncolored, start DFS from this node with color 1
+                if (!dfs(graph, colors, i, 1)) {
                     return false;
                 }
             }
         }
+
         return true;
     }
 }
