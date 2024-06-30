6f90ad77e83fd52d69838cee456824ffb17c315c
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2024-06-30 22:39:47 +0300

    Time: 641 ms (74.75%), Space: 18.1 MB (27.58%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -3,23 +3,24 @@ class Solution:
         rows, cols = len(maze), len(maze[0])
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-        queue = deque([(entrance[0], entrance[1], 0)])  # (row, col, steps)
-        visited = set()
-        visited.add((entrance[0], entrance[1]))
+        queue = deque()  
+        queue.append((entrance[0], entrance[1], 0)) # (row, col, steps)
+        visitedCell = set()
+        visitedCell.add((entrance[0], entrance[1]))
 
         while queue:
-            row, col, steps = queue.popleft()
+            row, col, numberOfSteps = queue.popleft()
 
             for dr, dc in directions:
-                new_row, new_col = row + dr, col + dc
+                newRow, newCol = row + dr, col + dc
 
-                if 0 <= new_row < rows and 0 <= new_col < cols and (new_row, new_col) not in visited:
-                    if maze[new_row][new_col] == '.':
+                if 0 <= newRow < rows and 0 <= newCol < cols and (newRow, newCol) not in visitedCell:
+                    if maze[newRow][newCol] == '.':
                         # Check if it's an exit (on the border and not the entrance)
-                        if (new_row == 0 or new_row == rows - 1 or new_col == 0 or new_col == cols - 1) and [new_row, new_col] != entrance:
-                            return steps + 1
+                        if (newRow == 0 or newRow == rows - 1 or newCol == 0 or newCol == cols - 1) and [newRow, newCol] != entrance:
+                            return numberOfSteps + 1
                         # Otherwise, add to the queue for further exploration
-                        queue.append((new_row, new_col, steps + 1))
-                        visited.add((new_row, new_col))
+                        queue.append((newRow, newCol, numberOfSteps + 1))
+                        visitedCell.add((newRow, newCol))
 
         return -1    
