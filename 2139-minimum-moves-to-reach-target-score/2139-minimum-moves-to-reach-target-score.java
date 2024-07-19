2d9018ccb1408481cd3ee016aeb8b0cb1eae7016
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2024-07-19 21:56:09 +0300

    Time: 0 ms (100.00%), Space: 40.3 MB (56.69%) - LeetHub

--- 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.java
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.java
@@ -2,13 +2,15 @@ public class Solution {
     public int minMoves(int target, int maxDoubles) {
         int numberOfMovesToOne = 0;
         while (target > 1) {
-            if (target % 2 == 0 && maxDoubles > 0) {
-                target /= 2;  
-                maxDoubles--; 
-            } 
-            else if (target % 2 == 1 && maxDoubles > 0) {
-                target--; 
-            } 
+            if (maxDoubles > 0) {
+                if(target % 2 == 0){
+                    target /= 2;
+                    maxDoubles--;
+                }
+                else if(target % 2 == 1){
+                    target--;
+                }
+            }  
             else {
                 numberOfMovesToOne += (target - 1); 
                 break; 
