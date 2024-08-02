f555ce4d9be68c137500f2d62df8ba1780ddb2bd
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2024-08-02 10:11:39 +0300

    Time: 385 ms (22.74%), Space: 26 MB (58.12%) - LeetHub

--- 0851-loud-and-rich/0851-loud-and-rich.py
+++ 0851-loud-and-rich/0851-loud-and-rich.py
@@ -1,6 +1,7 @@
 class Solution:
     def loudAndRich(self, richer: List[List[int]], quiet: List[int]) -> List[int]:
         n=len(quiet)
+        
         def dfs(graph,source,quietness,output):
             leastQuiet=source
             for neighbor in graph[source]:
@@ -8,10 +9,15 @@ class Solution:
                     dfs(graph,neighbor,quietness,output)
                 leastQuiet=min(leastQuiet,output[neighbor],key=lambda x:quietness[x])
             output[source]=leastQuiet
+            
+            
         graph=[set() for i in range(n)]
+        
         for relation in richer:
             graph[relation[1]].add(relation[0])
+            
         output=[None for i in range(n)]
+        
         for person in range(n):
             if output[person] is None:
                 dfs(graph,person,quiet,output)
