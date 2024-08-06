2940c22368c84016cc324ee71fc1252f54c62277
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2024-08-06 20:44:27 +0300

    Time: 640 ms (5.07%), Space: 16.4 MB (88.71%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        memo={}
+        if n in memo:
+            return memo[n]
+        if n<=1:
+            return n
+        memo[n] = self.fib(n - 1) + self.fib(n - 2)
+        return memo[n]
+        
