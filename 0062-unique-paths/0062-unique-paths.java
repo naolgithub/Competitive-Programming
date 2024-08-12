01dfd3159a598d5fdbc74d9a87a4274c262f5845
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2024-08-12 20:54:31 +0300

    Time: 0 ms (100.00%), Space: 40 MB (85.78%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int dp[][] = new int[m][n];
+        for(int row=0;row<m;row++){
+            dp[row][0]=1;
+        }
+
+        for(int col=0;col<n;col++){
+            dp[0][col]=1;
+        }
+
+        for(int row=1;row<m;row++){
+            for(int col=1;col<n;col++){
+                dp[row][col]=dp[row-1][col]+dp[row][col-1];
+            }
+        }
+
+        return dp[m-1][n-1];
+    } 
+}
