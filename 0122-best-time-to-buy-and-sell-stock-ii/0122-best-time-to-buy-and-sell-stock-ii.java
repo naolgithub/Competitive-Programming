8d1551859f63fc91c48e0825334812cef3c72bab
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2024-08-20 23:14:12 +0300

    Time: 1 ms (90.00%), Space: 45.9 MB (31.90%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit=0;
+        for(int i=0;i<prices.length-1;i++){
+            if(prices[i+1]>prices[i]){
+                int currentProfit=prices[i+1]-prices[i];
+                maxProfit=maxProfit+currentProfit;
+            }
+        }
+        return maxProfit;
+    }
+}
