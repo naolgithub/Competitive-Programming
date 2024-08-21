d8f8fa99027962c0b37af262011e2a77139d39aa
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2024-08-21 20:06:29 +0300

    Time: 36 ms (79.19%), Space: 44.2 MB (54.38%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int[] dp = new int[nums.length];
+        int maxLength = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            dp[i] = 1;
+        }
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+        return maxLength;
+    }
+}
