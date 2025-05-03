b356d57135ad3c2d0d3d6cb797aa441be41638d2
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2025-05-03 17:40:49 +0300

    Time: 7 ms (50.8%), Space: 17.9 MB (28.3%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.py
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.py
@@ -1,9 +1,27 @@
 class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
+        # Brute force
+
+        # resultant_array=[words[0]]
+        # for i in range(1,len(words)):
+        #     if sorted(words[i])!=sorted(words[i-1]):
+        #         resultant_array.append(words[i])
+        # return resultant_array
+
+        # Optimized approach
+        def count_chars(word):
+            freq=[0]*26
+            for char in word:
+                freq[ord(char)-ord('a')]+=1
+            return freq
         resultant_array=[words[0]]
+        previous_count=count_chars(words[0])
         for i in range(1,len(words)):
-            if sorted(words[i])!=sorted(words[i-1]):
+            current_count=count_chars(words[i])
+            if current_count!=previous_count:
                 resultant_array.append(words[i])
+                previous_count=current_count
         return resultant_array
+
 
         
