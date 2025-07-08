3c305314c4a072e5b00321fa6faf163b7144bd26
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2025-07-08 12:31:48 +0300

    Time: 0 ms (100%), Space: 17.7 MB (56.43%) - LeetHub

--- 3226-minimum-number-game/3226-minimum-number-game.py
+++ 3226-minimum-number-game/3226-minimum-number-game.py
@@ -2,13 +2,16 @@ import heapq
 
 class Solution:
     def numberGame(self, nums: List[int]) -> List[int]:
-        heapq.heapify(nums)  
-        result = []
+        heapq.heapify(nums)  #tc:N, sc:1
+        result = [] #sc:N tc:1
 
-        while len(nums) > 1:
+        while len(nums) > 1: #tc:NlogN sc:1
             first = heapq.heappop(nums)   # smallest
             second = heapq.heappop(nums)  # second smallest
             result.append(second)
             result.append(first)
 
         return result
+
+        #tc:NlogN
+        #sc:N
