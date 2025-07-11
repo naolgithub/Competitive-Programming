d126426ca49a7748a8c019fc01ea3e6cef51ff9c
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2025-07-11 17:34:16 +0300

    Time: 15 ms (5.32%), Space: 18.3 MB (6.33%) - LeetHub

--- 2350-find-closest-number-to-zero/2350-find-closest-number-to-zero.py
+++ 2350-find-closest-number-to-zero/2350-find-closest-number-to-zero.py
@@ -7,8 +7,10 @@ class Solution:
         #     elif abs(num) == abs(closest) and num > closest:
         #         closest = num
         # return closest
+        # tc: N sc:1
         min_heap = []
         for num in nums:
             heapq.heappush(min_heap, (abs(num), -num))
         return -min_heap[0][1]
+        #tc: NlogN sc:N
         
