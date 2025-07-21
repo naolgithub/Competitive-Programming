f46652a9dcc53bd5d289702c1ed7fcc350fcbe5c
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2025-07-21 10:51:03 +0300

    Time: 44 ms (44.26%), Space: 17.8 MB (23.1%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -5,14 +5,25 @@ class Solution:
         #     return n
         # return self.fib(n-1)+self.fib(n-2)
 
-        #Top-down
-        memo={}
-        def fibo_helper(num):
-            if num<=1:
-                return num
-            if num in memo:
-                return memo[num]
-            memo[num]=fibo_helper(num-1)+fibo_helper(num-2)
-            return memo[num]
-        return fibo_helper(n)
+        # #Top-down
+        # memo={}
+        # def fibo_helper(num):
+        #     if num<=1:
+        #         return num
+        #     if num in memo:
+        #         return memo[num]
+        #     memo[num]=fibo_helper(num-1)+fibo_helper(num-2)
+        #     return memo[num]
+        # return fibo_helper(n)
+
+        #Tabulation with 0(1) space
+        if n<=1:
+            return n
+        prev2=0
+        prev1=1
+        for i in range(2,n+1):
+            curr=prev2+prev1
+            prev2=prev1
+            prev1=curr
+        return prev1 
         
