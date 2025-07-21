ffff67cf9a334c0373c3b4e69d97d63acd049ff9
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2025-07-21 16:24:51 +0300

    Time: 0 ms (100%), Space: 18.6 MB (73.4%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -1,12 +1,25 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left,right=0,len(nums)-1
-        while left<=right:
-            mid=(left+right)//2
+        # #binary search
+        # left,right=0,len(nums)-1
+        # while left<=right:
+        #     mid=(left+right)//2
+        #     if nums[mid]==target:
+        #         return mid
+        #     elif nums[mid]<target:
+        #         left=mid+1
+        #     else:
+        #         right=mid-1
+        # return -1
+        # binary search with recursion(divide and conquer)
+        def recursive_binary_search(nums,target,left,right):
+            if left>right:
+                return -1
+            mid=(left+right)//2 #integer division which does not return float
             if nums[mid]==target:
                 return mid
-            elif nums[mid]<target:
-                left=mid+1
+            if nums[mid]<target:
+                return recursive_binary_search(nums,target,mid+1,right)
             else:
-                right=mid-1
-        return -1
+                return recursive_binary_search(nums,target,left,mid-1)
+        return recursive_binary_search(nums,target,0,len(nums)-1)
