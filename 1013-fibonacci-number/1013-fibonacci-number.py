7b95255c90ee1873b4c940a7447e902dea37d555
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2025-07-21 10:46:46 +0300

    Time: 32 ms (91.29%), Space: 17.9 MB (23.1%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,6 +1,18 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n<=1:
-            return n
-        return self.fib(n-1)+self.fib(n-2)
+        # # Recursion
+        # if n<=1:
+        #     return n
+        # return self.fib(n-1)+self.fib(n-2)
+
+        #Top-down
+        memo={}
+        def fibo_helper(num):
+            if num<=1:
+                return num
+            if num in memo:
+                return memo[num]
+            memo[num]=fibo_helper(num-1)+fibo_helper(num-2)
+            return memo[num]
+        return fibo_helper(n)
         
