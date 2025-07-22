944fad37166bf73126c72bd33f00777aedfbf4c2
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2025-07-22 21:35:29 +0300

    Time: 0 ms (100%), Space: 17.7 MB (61.53%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.py
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.py
@@ -1,13 +1,14 @@
 class Solution:
     def numberOfSteps(self, num: int) -> int:
-        def steps(num,count):
-            if num==0:
-                return count
-            if num%2==0:
-                return steps(num//2,count+1)
-            if num%2==1:
-                return steps(num-1,count+1)
-        return steps(num,0)
+        #Recursive approach
+        # def steps(num,count):
+        #     if num==0:
+        #         return count
+        #     if num%2==0:
+        #         return steps(num//2,count+1)
+        #     if num%2==1:
+        #         return steps(num-1,count+1)
+        # return steps(num,0)
         #tc:log2N sc:log2N
         # similar to (count zeros) of kunal kushwaha
         # 14/2=7,count=1
@@ -16,3 +17,14 @@ class Solution:
         # 3-1=2,count=4
         # 2/2=1,count=5
         # 1/2=0,count=6
+
+        #iterative approach
+        #tc:0(logN), sc:0(1)
+        count_zero=0
+        while num>0:
+            if num%2==0:
+                num//=2
+            else:
+                num-=1
+            count_zero+=1
+        return count_zero
