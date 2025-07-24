acdb2f38135675a791d95949d89005a8fdf3a7cd
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2025-07-24 23:01:25 +0300

    Time: 39 ms (57.78%), Space: 17.6 MB (83.49%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.py
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.py
@@ -1,14 +1,16 @@
 class Solution:
     def countOperations(self, num1: int, num2: int) -> int:
-        def count_operations(num1,num2,count):
-            if num1==0 or num2==0:
-                return count
-            if num1>=num2:
-                return count_operations(num1-num2,num2,count+1)
-            else:
-                return count_operations(num1,num2-num1,count+1)
-        return count_operations(num1,num2,0)
-        
+        #recursive approach
+
+        # def count_operations(num1,num2,count):
+        #     if num1==0 or num2==0:
+        #         return count
+        #     if num1>=num2:
+        #         return count_operations(num1-num2,num2,count+1)
+        #     else:
+        #         return count_operations(num1,num2-num1,count+1)
+        # return count_operations(num1,num2,0)
+
         # tc=sc=0(max(num1,num2))
 
         # num1 = 2, num2 = 3
@@ -18,5 +20,18 @@ class Solution:
         # num1 = 10, num2 = 10
         # count_operations(10,10,0)
         # count_operations(0,10,1)
+
+        # iterative approach
+        count=0
+        while num1>0 and num2>0:
+            if num1>=num2:
+                num1=num1-num2
+            else:
+                num2=num2-num1
+            count+=1
+        return count
+        #tc:0(max(num1,num2))
+        #sc:0(1)
+
 
         
