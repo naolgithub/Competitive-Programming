fbb229721db620fa09c8c354dcfcf45289cc72fb
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2025-07-24 22:36:17 +0300

    Time: 111 ms (5.14%), Space: 38.7 MB (11.91%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.py
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.py
@@ -8,12 +8,13 @@ class Solution:
             else:
                 return count_operations(num1,num2-num1,count+1)
         return count_operations(num1,num2,0)
+        
+        # tc=sc=0(max(num1,num2))
 
         # num1 = 2, num2 = 3
         # count_operations(2,3-2,1) count_operations(2,1,1)
         # count_operations(1,1,2)
         # count_operations(0,1,3)
-
         # num1 = 10, num2 = 10
         # count_operations(10,10,0)
         # count_operations(0,10,1)
