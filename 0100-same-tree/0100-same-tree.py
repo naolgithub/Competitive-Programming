7aae30ac1cb3c32d159061578494482f963fa5ce
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2025-07-28 00:46:16 +0300

    Time: 0 ms (100%), Space: 17.6 MB (98.93%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -4,11 +4,27 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
-            return True
-        if not p or not q:
-            return False
-        return p.val==q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
-        
+        #dfs
+        # if not p and not q:
+        #     return True
+        # if not p or not q:
+        #     return False
+        # return p.val==q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+
+        #bfs
+        queue=deque([(p,q)])
+        while queue:
+            for _ in range(len(queue)):
+                nodep, nodeq = queue.popleft()
+                if not nodep and not nodeq:
+                    continue
+                if not nodep or not nodeq:
+                    return False
+                if nodep.val!=nodeq.val:
+                    return False
+                queue.append((nodep.left,nodeq.left))
+                queue.append((nodep.right,nodeq.right))
+        return True
