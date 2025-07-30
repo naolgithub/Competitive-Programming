1866e3aa24054d7190e04553eff31b34d0505735
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2025-07-30 12:57:32 +0300

    Time: 0 ms (100%), Space: 17.7 MB (83.79%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -4,13 +4,31 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        def dfs(leftNode,rightNode):
-            if not leftNode and not rightNode:
-                return True
-            if not leftNode or not rightNode:
-                return False
-            return leftNode.val==rightNode.val and dfs(leftNode.left,rightNode.right) and dfs(leftNode.right, rightNode.left)
-        return dfs(root.left,root.right)
-        
+        #dfs
+        # def dfs(leftNode,rightNode):
+        #     if not leftNode and not rightNode:
+        #         return True
+        #     if not leftNode or not rightNode:
+        #         return False
+        #     return leftNode.val==rightNode.val and dfs(leftNode.left,rightNode.right) and dfs(leftNode.right, rightNode.left)
+        # return dfs(root.left,root.right)
+
+        #iterative approach (bfs)
+        if not root:
+            return True
+        queue=deque([(root.left,root.right)])
+        while queue:
+            for _ in range(len(queue)):
+                leftNode,rightNode=queue.popleft()
+                if not leftNode and not rightNode:
+                    continue
+                if not leftNode or not rightNode:
+                    return False
+                if leftNode.val!=rightNode.val:
+                    return False
+                queue.append([leftNode.left,rightNode.right])
+                queue.append([leftNode.right,rightNode.left])
+        return True
