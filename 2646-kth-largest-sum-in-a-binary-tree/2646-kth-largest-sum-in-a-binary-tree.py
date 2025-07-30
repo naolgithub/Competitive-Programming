7fc23512575e288d3d5e5875914935f86a6f2151
Naol Abera <93266524+naolgithub@users.noreply.github.com>
2025-07-30 19:52:36 +0300

    Time: 170 ms (52.08%), Space: 55 MB (44.76%) - LeetHub

--- 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.py
+++ 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.py
@@ -4,26 +4,49 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
+import heapq
 class Solution:
     def kthLargestLevelSum(self, root: Optional[TreeNode], k: int) -> int:
+        #bfs+sorting approach
+        # queue=deque([root])
+        # depth=0
+        # level_sums=[]
+        # while queue:
+        #     current_level_sum=0
+        #     for _ in range(len(queue)):
+        #         node = queue.popleft()
+        #         current_level_sum+=node.val
+        #         if node.left:
+        #             queue.append(node.left)
+        #         if node.right:
+        #             queue.append(node.right)
+        #     level_sums.append(current_level_sum)
+        #     depth+=1
+        # level_sums.sort(reverse=True)
+        # if depth<k:
+        #     return -1
+        # return level_sums[k-1]
+
+        #bfs+min_heap
         queue=deque([root])
+        min_heap=[]
         depth=0
-        kth_sum=-1
-        level_sums=[]
         while queue:
             current_level_sum=0
             for _ in range(len(queue)):
-                node = queue.popleft()
+                node=queue.popleft()
                 current_level_sum+=node.val
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
-            level_sums.append(current_level_sum)
             depth+=1
-        level_sums.sort(reverse=True)
+            heapq.heappush(min_heap,current_level_sum)
+            while len(min_heap)>k:
+                heapq.heappop(min_heap)
         if depth<k:
             return -1
-        return level_sums[k-1]
+        return min_heap[0] 
                 
         
